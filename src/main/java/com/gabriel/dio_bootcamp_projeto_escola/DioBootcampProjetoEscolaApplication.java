81c270bed73f1e7222c7102e191e06f8a4bd9eb9
GabrielF-S <gabrielboni2008@hotmail.com>
2025-03-09 18:19:36 -0300

    Initial commit

---
+++ src/main/java/com/gabriel/dio_bootcamp_projeto_escola/DioBootcampProjetoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.dio_bootcamp_projeto_escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBootcampProjetoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBootcampProjetoEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/dio_bootcamp_projeto_escola/DioBootcampProjetoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.dio_bootcamp_projeto_escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBootcampProjetoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
